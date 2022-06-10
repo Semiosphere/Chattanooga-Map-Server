fb26aad625bf99f6193014d248caf8c7816f42bc
Zach Dugger <ztd522@gmail.com>
2022-06-10 16:42:37 -0400

    created fixture for profile pics with urls added

--- app_api/models/location.py
+++ app_api/models/location.py
@@ -4,7 +4,8 @@ class Location(models.Model):
     name = models.CharField(max_length=50)
     coordinates = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
-    img = models.URLField()
+    map_icon = models.URLField()
+    character_art = models.URLField()
     discovered_by = models.ManyToManyField("Profile", related_name="locations")
     
     @property
