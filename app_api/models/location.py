5930de737ac38a10d57d47dd1e41c109a9c22a89
Zach Dugger <ztd522@gmail.com>
2022-06-23 16:03:11 -0400

    increasing location model description char limit to 1000

--- app_api/models/location.py
+++ app_api/models/location.py
@@ -3,7 +3,7 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=50)
     coordinates = models.CharField(max_length=50)
-    description = models.CharField(max_length=300)
+    description = models.CharField(max_length=1000)
     character_art = models.URLField()
     discovered_by = models.ManyToManyField("Profile", related_name="locations")
     x = models.IntegerField()
