62428cb5d0f0e48aaacd7628e3ff999e46bf17eb
Zach Dugger <ztd522@gmail.com>
2022-06-23 15:47:16 -0400

    trying to deploy server

--- app_project/settings.py
+++ app_project/settings.py
@@ -1,4 +1,6 @@
 import os
+import django_on_heroku
+
 
 """
 Django settings for app_project project.
@@ -142,3 +144,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+django_on_heroku.settings(locals())
